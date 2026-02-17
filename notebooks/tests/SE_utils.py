4279e3d917f8538c53fbac5492e2ed4e8a7f0c82
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-02-17 13:58:57 +0100

    backup of first real attempt to comptue S-RSE LDOS - does not converge as nicely as Alans version

--- notebooks/tests/SE_utils.py
+++ notebooks/tests/SE_utils.py
@@ -21,21 +21,21 @@ def calc_GF(E,eta,rse,H_sub,S_sub,alist=None):
     else: 
         A -= RSE
     G = np.linalg.inv(A)   
-    dos = - (1.0 / np.pi) * np.imag(np.trace(G@S_sub)) 
     ldos = - (1.0 / np.pi) * np.imag(np.diag(G@S_sub))
-    return dos, ldos
+    # dos = - (1.0 / np.pi) * np.imag(np.trace(G@S_sub)) 
+    return ldos
 
 
 
 def loop_GF(energies,eta,rse,H_sub,S_sub,alist=None):
     # Observables
     N = H_sub.shape[0]
-    DOS = np.zeros_like(energies, dtype=float)
+    # DOS = np.zeros_like(energies, dtype=float)
     LDOS = np.zeros((N, len(energies)), dtype=float)  
 
     # Loop
     for iE, E in enumerate(tqdm(energies, desc='DOS')):
-        dos,ldos =calc_GF(E,eta,rse,H_sub,S_sub,alist=alist)
-        DOS[iE] = dos
+        ldos = calc_GF(E,eta,rse,H_sub,S_sub,alist=alist)
+        # DOS[iE] = np.sum(ldos)
         LDOS[:,iE] = ldos
-    return DOS, LDOS
+    return LDOS
