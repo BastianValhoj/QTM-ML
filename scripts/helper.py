b4f87ebe45e3a945423d9f833bb1d76417f26443
Bastian Desktop-WSL <bastian.kristensen@outlook.com>
2026-04-13 11:52:48 +0200

    to avoid vs code linting error use the direct import from IPython.core.getipython instead of the indirect

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,7 +1,7 @@
 
 def in_notebook():
     try:
-        from IPython import get_ipython
+        from IPython.core.getipython import get_ipython
         shell = get_ipython().__class__.__name__
         return shell == "ZMQInteractiveShell" # if its a notebook, this comparison will return true
     except Exception:
