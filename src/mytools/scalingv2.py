d6d6fafb7ff80789e6ce2c6ea15415f1787b73dc
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-05-08 11:35:49 +0200

    fixed NC=0 and N_small=3 edge case

--- src/mytools/scalingv2.py
+++ src/mytools/scalingv2.py
@@ -22,7 +22,7 @@ def _tile_indices(n1: int, n2: int) -> tuple[int, int, int, int]:
 
 def get_corners(tile1, tile2=None, na=6, NC=1):
     if NC < 0:
-        raise ValueError("## NC must be > 0")
+        raise ValueError("## NC must be >= 0")
     if tile2 is None:
         tile2 = tile1
     BL, BR, TR, TL = _tile_indices(tile1, tile2)
@@ -33,7 +33,7 @@ def get_corners(tile1, tile2=None, na=6, NC=1):
         range(na*(TL - NC), na*(TL + NC + 1)), # top left hex + 2*NC
         range(na*(BL - NC), na*BL) # bottom left 1*NC
     ]
-    return np.concat(corners_atom_match)
+    return np.concat(corners_atom_match).astype(int)
 
 def map_corners(tiles1, tiles2, na=6, NC=1):
     if not isinstance(tiles1, (tuple, list)):
@@ -72,11 +72,13 @@ def get_centers(coords, na):
 
 def map_edges(g1, g2, tiles1, tiles2, na=6, NC=1):
     if isinstance(tiles1, (tuple, list)):
+        raise ValueError("We will only allow tiles1 being integer corresponding to tiling the same amount along vectors A and B")
         N1a, N1b = tiles1
     else:
         N1a = N1b = tiles1
     
     if isinstance(tiles2, (tuple, list)):
+        raise ValueError("We will only allow tiles2 being integer corresponding to tiling the same amount along vectors A and B")
         N2a, N2b = tiles2
     else:
         N2a = N2b = tiles2
@@ -92,8 +94,52 @@ def map_edges(g1, g2, tiles1, tiles2, na=6, NC=1):
     cent1_frac = get_fractional(g1, cent1)
     cent2_frac = get_fractional(g2, cent2)
     
+    # print(f"cent1 frac : {cent1_frac}")
+    # print(f"cent2 frac : {cent2_frac}")
+    # print()
+    # print(f"edges2 : {edges2}")
+    
     tree = cKDTree(cent1_frac)
     _, edge_idx1_to_idx2 = tree.query(cent2_frac, k=1)
+    # return edges1.reshape(-1, na), edge_idx1_to_idx2
+    
+    # get centroids
+    edges1 = get_edges(N1a, N1b, na, NC)
+    edges2 = get_edges(N2a, N2b, na, NC)
+    
+    # cent1 = get_centers(g1.xyz[edges1], na)
+    # cent2 = get_centers(g2.xyz[edges2], na)
+    # 
+    # # get fractional centroids
+    # cent1_frac = get_fractional(g1, cent1)
+    # cent2_frac = get_fractional(g2, cent2)
+    
+    # CORRECTION we will only support cases where Na=Nb so each side is equal length
+    n1_per_side = edges1.shape[0] // 4 // na # number of UC/centroids per side in small
+    n2_per_side = edges2.shape[0] // 4 // na # number of centroids per side in big
+    # print(n1_per_side)
+    # print(n2_per_side)
+    
+    # match only using kNN to centroids ON THE SAME EDGE!! otherwise some combination of tiles wrongfully match indices
+    edge_idx1_to_idx2 = np.empty(edges2.shape[0] // na, dtype=int)
+    for side in range(4):
+        e1_side  = edges1[side*n1_per_side : (side+1)*n1_per_side]
+        e2_side  = edges1[side*n1_per_side : (side+1)*n1_per_side]
+        
+        cent1_side = get_centers(g1.xyz[e1_side.flatten()], na)
+        cent2_side = get_centers(g2.xyz[e2_side.flatten()], na)
+        
+        cent1_frac = get_fractional(g1, cent1_side)
+        cent2_frac = get_fractional(g2, cent2_side)
+        
+        tree = cKDTree(cent1_frac)
+        _, idx = tree.query(cent2_frac, k=1)
+        # print("idx")
+        # print(idx[0])
+        edge_idx1_to_idx2[side*n2_per_side:(side+1)*n2_per_side] = idx[0] + side*n1_per_side
+    
+    # tree = cKDTree(cent1_frac)
+    # _, edge_idx1_to_idx2 = tree.query(cent2_frac, k=1)
     return edges1.reshape(-1, na), edge_idx1_to_idx2
 
 def _splitter(arr, diff_tol):
@@ -114,9 +160,19 @@ def extrapolate(g1, g2, tiles1, tiles2, na=6, NC=1):
     # get corners
     corners1 = get_corners(N1a, N1b, na, NC)
     corner_parts = _splitter(corners1, na)
+    # print(f"len(edge_idx1_to_idx2) : {len(edge_idx1_to_idx2)}")
+    # print(f"edge_idx1_to_idx2 : {edge_idx1_to_idx2}")
+    # print(f"corners1 : {corners1}")
+    # print(f"Number of corner parts : {len(corner_parts)}")
+    # for i, p in enumerate(corner_parts):
+    #     print(f"  corner_parts[{i}] : {p}")
     
     edge1_to_edges2 = edges1[edge_idx1_to_idx2]
     edge_parts = _splitter(edge1_to_edges2.flatten(), na)
+    
+    # print(f"Number of edge parts: {len(edge_parts)}")
+    # for i, p in enumerate(edge_parts):
+    #     print(f"  edge_parts[{i}]: {p}")
     
     
     # append to final list
@@ -129,8 +185,12 @@ def extrapolate(g1, g2, tiles1, tiles2, na=6, NC=1):
             edge_parts[2],
             corner_parts[3],
             edge_parts[3],
-            corner_parts[4],
+            # corner_parts[4],
         ])
+    if NC>0: # if NC=0 no 5'th element in the corner_parts, otherwise will be the last 'additional'/NC-corner for the starting postion.
+        g1_to_g2_idx = np.concat([g1_to_g2_idx, corner_parts[4]])
+    # else:
+        # print("NC = 0, so no additional extra NC-corner")
     return g1_to_g2_idx
 
 
