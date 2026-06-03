44350f1a37665dca0512e0c5ab39cf3711ab04b3
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-06-03 16:38:53 +0200

    for each nk1 save N vs DOS(E=0)

--- scripts/calc_dos_vs_nk1.py
+++ scripts/calc_dos_vs_nk1.py
@@ -11,13 +11,14 @@ DATA_DIR = SCRIPT_DIR / "conv_data"
 BOND = 1.42
 Vpppi = -2.7
 
-ENERGIES = [0]
+# ENERGIES = np.arange(0.4, -(0.4 + 0.05), step=-0.1)
+ENERGIES = 0.0
 
-NK1S_ENUM = np.arange(800, 1300+1, step=100)
+NK1S_ENUM = np.arange(300, 2000, step=300)
 ETA = 1e-3
 
-N_zig = [2*i+1 for i in range(1,4)][::-1]
-N_arm = [4*i+1 for i in range(1,4)][::-1]
+N_zig = [4*i+1 for i in range(1,8)][::-1]
+N_arm = [2*i+1 for i in range(1,8)][::-1]
 
 def make_hamiltonian(geom):
     
@@ -36,7 +37,7 @@ with h5py.File(DATA_DIR / "calc_dos_vs_nk1.h5", "w") as file:
     file.attrs["NK1_enum"] = NK1S_ENUM
     
     for kind in ["zigzag", "armchair"]:
-        tqdm.write("### kind")
+        tqdm.write("### kind : {}".format(kind))
         group_kind = file.create_group(kind)
         if kind == "zigzag":
             base_geom = sisl.geom.graphene(bond=BOND)
@@ -50,28 +51,19 @@ with h5py.File(DATA_DIR / "calc_dos_vs_nk1.h5", "w") as file:
         Ham0 = make_hamiltonian(base_geom)
         group_kind.attrs["N"] = Nlist
         
-        for N in tqdm(Nlist, desc="Looping over N"):
+        for nk1 in tqdm(NK1S_ENUM, desc="Looping over nk1", leave=True):
             
-            
-            rsse = sisl.RealSpaceSE(Ham0, 0, 1, (N, N, 1))
-            
-            DOSE0_vs_nk1 = []
-            for nk1 in tqdm(NK1S_ENUM, desc="looping nk1", leave=False):
+            group_nk1 = group_kind.create_dataset(f"nk1_enum{nk1}", shape=(len(Nlist),2), dtype=np.float128)
+            for idx, N in enumerate(tqdm(Nlist, desc="Looping over N"), leave=False):
+                
+                rsse = sisl.RealSpaceSE(Ham0, 0, 1, (N, N, 1))
                 rsse.setup(eta=ETA, bz=sisl.MonkhorstPack(Ham0, [1, np.ceil(nk1/N), 1]))
-                
-                Ham_elec, elec_idx = rsse.real_space_coupling(True)
-                Ham_NN = rsse.real_space_parent()
-                
-                # Hk = Ham_NN.Hk(dtype=np.complex128)
-                # Sk = Ham_NN.Sk(dtype=np.complex128)
-                
                 invG = rsse.self_energy(E=0.0, bulk=True)
-                GFs = np.linalg.inv(invG)
-                LDOS = (-1/np.pi)*np.diagonal(np.imag(GFs), axis1=0, axis2=1)
-                DOS = LDOS.sum(axis=-1)
-                DOSE0_vs_nk1.append(DOS)
-            DOSE0_vs_nk1 = np.array(DOSE0_vs_nk1)
-            group_kind.create_dataset(f"N{N}", data=DOSE0_vs_nk1)
+                GF = np.linalg.inv(invG)
+                LDOS = (-1/np.pi)*np.diagonal(np.imag(GF), axis1=0, axis2=1)
+                DOS = LDOS.sum()
+                group_nk1[idx] = (N, DOS)
+                
                 
 print("Done!")
         
