70bac58de9c69551e149f28901746fa08e5b6efa
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-06-08 15:18:10 +0200

    trivial changes -- only pushed to cleanup local changes

--- scripts/overnight/hpc/bilayer/submit_bilayer.sh
+++ scripts/overnight/hpc/bilayer/submit_bilayer.sh
@@ -1,7 +1,7 @@
 # LSBATCH: User input
 #!/bin/bash
 ### Job name (shown in queue and in email notificaitons)
-#BSUB -J bi-NC1-30
+#BSUB -J bi-NC0-30
 ### Specify which queue (here, just the HPC)
 #BSUB -q hpc
 ### Request flag: reserve 16GB available for the duration of the job
@@ -19,16 +19,16 @@
 ### mail notification
 #BSUB -u s192943@student.dtu.dk
 ### -- Specify the output and error file. %J is the job-id -- 
-#BSUB -o out.bi-NC1-30.%J.log
-#BSUB -e err.bi-NC1-30.%J.log
+#BSUB -o out.H-NC0-30.%J.log
+#BSUB -e err.H-NC0-30.%J.log
 
-# uv run write_bilayer.py --nc 0 --ns 3 --nb 50
-# uv run write_bilayer.py --nc 0 --ns 4 --nb 50
-uv run write_bilayer.py --nc 1 --ns 5 --nb 30
-uv run write_bilayer.py --nc 1 --ns 6 --nb 30
-uv run write_bilayer.py --nc 1 --ns 7 --nb 30
-uv run write_bilayer.py --nc 1 --ns 8 --nb 30
-uv run write_bilayer.py --nc 1 --ns 9 --nb 30
-uv run write_bilayer.py --nc 1 --ns 10 --nb 30
-uv run write_bilayer.py --nc 1 --ns 11 --nb 30
-uv run write_bilayer.py --nc 1 --ns 12 --nb 30
+uv run write_bilayer.py --nc 0 --ns 3 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 4 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 5 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 6 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 7 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 8 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 9 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 10 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 11 --nb 30  
+uv run write_bilayer.py --nc 0 --ns 12 --nb 30  
