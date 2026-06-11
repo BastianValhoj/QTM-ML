9b71e95560485a7c6ddcb0b725edb19604a736d4
Bastian laptop WSL <bastian.kristensen@outlook.com>
2026-06-11 17:14:56 +0200

    include more data points

--- scripts/calc_dos_vs_nk1.py
+++ scripts/calc_dos_vs_nk1.py
@@ -14,11 +14,11 @@ Vpppi = -2.7
 # ENERGIES = np.arange(0.4, -(0.4 + 0.05), step=-0.1)
 ENERGIES = 0.0
 
-NK1S_ENUM = np.arange(300, 2000, step=300)
+NK1S_ENUM = np.arange(300, 4001, step=300)
 ETA = 1e-3
 
-N_zig = [4*i+1 for i in range(1,8)][::-1]
-N_arm = [2*i+1 for i in range(1,8)][::-1]
+N_zig = [4*i+1 for i in range(2,7)]
+N_arm = [2*i+1 for i in range(2,7)]
 
 def make_hamiltonian(geom):
     
@@ -31,7 +31,7 @@ def make_hamiltonian(geom):
 
 
 gr_zig = sisl.geom.graphene(bond=BOND)
-with h5py.File(DATA_DIR / "calc_dos_vs_nk1.h5", "w") as file:
+with h5py.File(DATA_DIR / "calc_dos_vs_nk1_sampling.h5", "w") as file:
     file.attrs["E"] = ENERGIES
     file.attrs["ETA"] = ETA
     file.attrs["NK1_enum"] = NK1S_ENUM
@@ -54,17 +54,17 @@ with h5py.File(DATA_DIR / "calc_dos_vs_nk1.h5", "w") as file:
         for nk1 in tqdm(NK1S_ENUM, desc="Looping over nk1", leave=True):
             
             group_nk1 = group_kind.create_dataset(f"nk1_enum{nk1}", shape=(len(Nlist),2), dtype=np.float128)
-            for idx, N in enumerate(tqdm(Nlist, desc="Looping over N"), leave=False):
+            for idx, N in enumerate(tqdm(Nlist, desc="Looping over N", leave=False)):
                 
                 rsse = sisl.RealSpaceSE(Ham0, 0, 1, (N, N, 1))
                 rsse.setup(eta=ETA, bz=sisl.MonkhorstPack(Ham0, [1, np.ceil(nk1/N), 1]))
                 invG = rsse.self_energy(E=0.0, bulk=True)
                 GF = np.linalg.inv(invG)
                 LDOS = (-1/np.pi)*np.diagonal(np.imag(GF), axis1=0, axis2=1)
-                DOS = LDOS.sum()
+                Ham_NN = rsse.real_space_parent()
+                DOS = LDOS.sum() / Ham_NN.na
                 group_nk1[idx] = (N, DOS)
-                
-                
+
 print("Done!")
         
         
